baf00d782945eb6540acacfc7595eae26d637a45
Diego Andre Poli <diegoandrepoli@gmail.com>
2019-02-16 11:47:31 -0200

    Create index method

--- main.go
+++ main.go
@@ -12,12 +12,20 @@ import (
  */
 func main() {
     var router = mux.NewRouter()
+    router.HandleFunc("/", index).Methods("GET")
     router.HandleFunc("/keep", postKeep).Methods("POST")
     router.HandleFunc("/keep", handleKeep).Methods("GET")
     router.HandleFunc("/keep/{id}", handleKeepById).Methods("GET")
 
     fmt.Println("Running keep server!")
     log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+/**
+ * Main keep data
+ */
+func index(w http.ResponseWriter, r *http.Request){
+   fmt.Fprintln(w, "Welcome to Keep!")
 }
 
 /**
