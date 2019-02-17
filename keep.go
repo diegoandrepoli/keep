43506086bce49aaa5290b65eb05b7cb39626ccb7
Diego Andre Poli <diegoandrepoli@gmail.com>
2019-02-17 16:49:15 -0300

    Create keep model

---
+++ keep.go
@@ -0,0 +1,11 @@
+
+package main
+
+// The keep type
+type Keep struct {
+    ID       string   `json:"id,omitempty"`
+    Title    string   `json:"title,omitempty"`
+    Message  string   `json:"message,omitempty"`
+}
+
+
--- main.go
+++ main.go
@@ -9,13 +9,7 @@ import (
 )
 
 
-// The keep type
-type Keep struct {
-    ID       string   `json:"id,omitempty"`
-    Title    string   `json:"title,omitempty"`
-    Message  string   `json:"message,omitempty"`
-}
-
+//list of keep
 var keeps []Keep
 
 
